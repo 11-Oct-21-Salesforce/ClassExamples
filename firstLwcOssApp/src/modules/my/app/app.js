b4a6c5d1f7396bf1a6c33d51836358770a286695
Ben <benjamin.learn@revature.com>
2021-10-15 13:37:50 -0400

    template directive examples

--- firstLwcOssApp/src/modules/my/app/app.js
+++ firstLwcOssApp/src/modules/my/app/app.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    displayMarkup = true;
+    teams = [
+        {
+            teamName: 'Alchemy',
+            squadName: 'Codeforce'
+        },
+        {
+            teamName: 'Alchemy',
+            squadName: 'Rogue Squadron'
+        },
+        {
+            teamName: 'Synergy',
+            squadName: 'Team Seven'
+        },
+        {
+            teamName: 'Vanquish',
+            squadName: 'Vanquishing America\'s Llama Undead Empire'
+        }
+    ]
+}
